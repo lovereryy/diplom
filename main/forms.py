dea432ca86a17d0774c51bc0f961121cb4dd6468
superhugebetformiladze <broton07@yandex.ru>
2025-03-19 22:47:17 +0400

    profile

--- main/forms.py
+++ main/forms.py
@@ -14,14 +14,16 @@ class ReviewForm(forms.ModelForm):
 class CustomUserChangeForm(forms.ModelForm):
     class Meta:
         model = User
-        fields = ["username", "email"]  # Только имя и почта
+        fields = ["username", "email", "first_name"]
         labels = {
-            "username": "Имя",
+            "username": "Никнейм",
             "email": "Email",
+            "first_name": "Имя",
         }
         widgets = {
             "username": forms.TextInput(attrs={"class": "form-control"}),
             "email": forms.EmailInput(attrs={"class": "form-control"}),
+            "first_name": forms.TextInput(attrs={"class": "form-control"}),
         }
 
     def __init__(self, *args, **kwargs):
@@ -29,6 +31,7 @@ class CustomUserChangeForm(forms.ModelForm):
         # Автоматически заполняем поля текущими данными пользователя
         self.fields["username"].initial = self.instance.username
         self.fields["email"].initial = self.instance.email
+        self.fields["first_name"].initial = self.instance.first_name
 
 class CustomAuthenticationForm(AuthenticationForm):
     username = forms.CharField(
